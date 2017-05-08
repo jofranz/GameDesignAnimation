8d5b6dc2b411a2e503cc8a8ef5cd0cb13494f566
Johannes Franz <jofranz90@gmail.com>
2017-05-08 17:33:35 +0200

    Added fixed textfield. Text can be changed. Text changes when onTrigger calls a method. Changeg path of keystore.

--- Rockstar/Assets/Scripts/ItemTrigger.cs
+++ Rockstar/Assets/Scripts/ItemTrigger.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class ItemTrigger : MonoBehaviour {
 
 
+	public string text = "method call - something happend";
+	public bool showText = false;
+
+	public Text showTextLabel;
 	// Use this for initialization
 	void Start () {
-		
+		showTextLabel.text = "emptyyy";
 	}
 	
 	// Update is called once per frame
@@ -18,9 +24,29 @@ public class ItemTrigger : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D target) {
 		print ("enter box " + Time.fixedTime);
+
+		showTextLabel.text = "erschlagen";
+
+		showTextNow ();
 	}
 
-	void OnTriggerExit2D(Collider2D target) {
-		print ("exit box " + Time.fixedTime);
+//	void OnTriggerExit2D(Collider2D target) {
+//		print ("exit box " + Time.fixedTime);
+//	}
+
+
+	void showTextNow() {
+		//showText = true;
 	}
+
+	void OnGUI(){
+		if (showText) {
+			GUI.Box (new Rect (0, Screen.height/2, Screen.width, Screen.height), text + "\nneue Zeile");
+			if (Input.GetMouseButtonDown (0)) {
+				showText = false; 
+			}
+		}
+	}
+
+
 }
