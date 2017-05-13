983a60a1e4fd0e6a868282e49ba6f425d48ddb2b
Johannes Franz <jofranz90@gmail.com>
2017-05-13 10:21:55 +0200

    Implemented short-click and long-press on game-objects in ItemInteraction class for unity and android. Added a door for further work

--- Rockstar/Assets/Scripts/ItemInteraction.cs
+++ Rockstar/Assets/Scripts/ItemInteraction.cs
@@ -11,6 +11,12 @@ public class ItemInteraction : MonoBehaviour {
 	private GuiLabel GuiLabelScript;
 	private GameObject GuiLabelGO;
 
+	private float startTime = 0.0f;
+	private float pressDuration = 1.0f;
+	private float timePassed = 0.0f;
+	private bool isPressed = false;
+	
+	private string targetName;
 
 	// Use this for initialization
 	void Start () {
@@ -18,30 +24,75 @@ public class ItemInteraction : MonoBehaviour {
 		ShowTextScript = TextControllerGO.GetComponent<ShowText>();
 
 
-
 		GuiLabelGO = GameObject.Find ("Main Camera");
 		GuiLabelScript = GuiLabelGO.GetComponent<GuiLabel> ();
+
+		targetName = gameObject.transform.name;
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (isPressed) {
+			timePassed = Time.time - startTime;
+			print ("timePassed: " + timePassed);
+
+			// zeit abgelaufen?
+			if ((timePassed > pressDuration) ) {
+				print ("Sekunde/n abgelaufen");
+
+				ShowTextScript.setText (targetName + " long pressed " + Time.time);
+
+				resetTimer ();
+			}
+
+		}
+
+
+
+
 	}
 		
 
 	void OnMouseOver() {
 
-		if (Input.GetMouseButtonDown (0) ) {
+		if (Input.GetMouseButtonDown (0)) {
+
+			if (startTime == 0.0F) {
+				startTime = Time.time;
+				print ("time set");
+			}
+
+			isPressed = true;
+
+
+
+
+			print ("startTime: " + startTime + " pressDuration: " + pressDuration + " timePassed: " + timePassed);
+
+
+
+
 			GuiLabelScript.showBox (true);
-			//clickedBefore = true;
-			string targetName = gameObject.transform.name;
-			ShowTextScript.setText (targetName + " angeklickt " + Time.fixedTime);
+
+			ShowTextScript.setText (targetName + " angeklickt " + Time.time);
 		} 
 
-//		if(Input.GetMouseButtonDown (0) && clickedBefore){
-//			GuiLabelScript.showBox (false);
-//			clickedBefore = true;
-//		}
+		if (Input.GetMouseButtonUp (0)) {
+			resetTimer ();
+		}
+
 	}
+	
+
+	void OnMouseExit() {
+		resetTimer ();
+	}
+
+	void resetTimer() {
+		isPressed = false;
+		startTime = 0.0F;
+		print ("resetTimer");
+	}
+
 }
