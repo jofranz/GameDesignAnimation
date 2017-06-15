982af1ac5b9aeafbd69e2d3dbe6404fc6654e3ab
Johannes Franz <jofranz90@gmail.com>
2017-06-15 22:29:40 +0200

    Added intro sequence. Reactivated music. Changed oder of scenes.

---
+++ Rockstar/Assets/Scripts/SkipSlides.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipSlides : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if ( Input.GetMouseButtonUp (0) ) {
+			print ("skip slide");
+			StartCoroutine (ChangeLevel () );
+		}
+
+	}
+
+
+
+	// fade to black
+	IEnumerator ChangeLevel() {
+		float mFadeTime = GameObject.Find("Main Camera").GetComponent<Fade> ().beginFade (1);
+		yield return new WaitForSeconds (mFadeTime);
+
+		SceneManager.LoadScene ("CutScene", LoadSceneMode.Single);
+	}
+}
