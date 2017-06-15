1cd23220f26f8be4031dc28521af5bfada330853
Johannes Franz <jofranz90@gmail.com>
2017-06-15 14:03:42 +0200

    Added background audio and a intro scene.

---
+++ Rockstar/Assets/PersistentSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentSound : MonoBehaviour {
+
+	private static PersistentSound instance = null;
+	public static PersistentSound Instance {
+		get { return instance; }
+	}
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
