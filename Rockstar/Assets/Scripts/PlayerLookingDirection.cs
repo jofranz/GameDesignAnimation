8d12c37a96fbf18733429c0bdf58151791a61755
Johannes Franz <jofranz90@gmail.com>
2017-06-17 22:34:58 +0200

    Fixed idle eye animation. Prepared puke animation.

--- Rockstar/Assets/Scripts/PlayerLookingDirection.cs
+++ Rockstar/Assets/Scripts/PlayerLookingDirection.cs
@@ -3,33 +3,37 @@ using System.Collections;
 
 public class PlayerLookingDirection : MonoBehaviour {
 
-
 	private Rigidbody2D rg;
-	private Animator animator;
+	private Animator mAnimator;
 	private float prevPosX = 0;
 	private Vector2 position;
 
 	void Start(){
-		animator = GetComponent<Animator> ();
+		mAnimator = GetComponent<Animator> ();
 		rg = GetComponent<Rigidbody2D>();
+
 	}
+
+
 
 	// Update is called once per frame
 	void Update () {
 
+		//mAnimator.SetInteger ("AnimState", 40); // 40 = puke animation
+		print ("puke");
+		mAnimator.SetTrigger ("doPuke");
 
-
-
+		if(false) {
 		// checks and animates player if moving
 		if (rg.position.x != prevPosX) {
-			// print("anim state change 1");
-			animator.SetInteger ("AnimState", 1);
-		} else {
-			animator.SetInteger("AnimState", 0);
-			// print("anim state change 0");
+			print("anim state change 1"); // walking animation
+				mAnimator.SetInteger ("AnimState", 1);
+		} else { // standing animation
+				mAnimator.SetInteger("AnimState", 0);
+			print("anim state change 0");
 		}
-			
+		}
 		prevPosX = rg.position.x;
+		
 	}
 }
-	
